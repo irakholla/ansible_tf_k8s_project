30f0aa61ce183cf2b4fe03be448dd591ae3d2823
rakholla.ilya <rakholla.ilya@gmail.com>
2022-04-06 02:14:42 +0300

    test yml

--- run_test_playbook.py
+++ run_test_playbook.py
@@ -3,4 +3,4 @@ import os
 ansible_playbook_path = "/Users/Ilia_Rakholla/tf_k8s_project/ansible/ansible_tf_k8s_project"
 
 os.chdir(ansible_playbook_path)
-os.system("/Users/Ilia_Rakholla/Library/Python/3.9/bin/ansible-playbook k8s.yml")
+os.system("/Users/Ilia_Rakholla/Library/Python/3.9/bin/ansible-playbook test_playbook.yml")
